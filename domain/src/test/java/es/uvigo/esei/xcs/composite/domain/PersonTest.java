ba7128383b8ad308178e1d32abcf13b21703069c
JOBFER1 <jobfer@outlook.es>
2023-05-18 11:31:17 +0200

    Revert "Modifica prueba para que falle"
    This reverts commit 66c737b816f27d11bc9c66b80dcb1499a8607add.

--- domain/src/test/java/es/uvigo/esei/xcs/composite/domain/PersonTest.java
+++ domain/src/test/java/es/uvigo/esei/xcs/composite/domain/PersonTest.java
@@ -15,8 +15,7 @@ public class PersonTest {
 		
 		final Person person = new Person(name, surname, phone);
 		
-//		assertThat(person.getName(), is(equalTo(name)));
-		assertThat(person.getName(), is(equalTo("Pepe")));
+		assertThat(person.getName(), is(equalTo(name)));
 		assertThat(person.getSurname(), is(equalTo(surname)));
 		assertThat(person.getPhone(), is(equalTo(phone)));
 	}
