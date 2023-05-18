66c737b816f27d11bc9c66b80dcb1499a8607add
JOBFER1 <jobfer@outlook.es>
2023-05-18 11:28:36 +0200

    Modifica prueba para que falle

--- domain/src/test/java/es/uvigo/esei/xcs/composite/domain/PersonTest.java
+++ domain/src/test/java/es/uvigo/esei/xcs/composite/domain/PersonTest.java
@@ -15,7 +15,8 @@ public class PersonTest {
 		
 		final Person person = new Person(name, surname, phone);
 		
-		assertThat(person.getName(), is(equalTo(name)));
+//		assertThat(person.getName(), is(equalTo(name)));
+		assertThat(person.getName(), is(equalTo("Pepe")));
 		assertThat(person.getSurname(), is(equalTo(surname)));
 		assertThat(person.getPhone(), is(equalTo(phone)));
 	}
