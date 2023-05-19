bd39758fb466ad1e6d6fc179bfab186af8c9bf44
JOBFER1 <jobfer@outlook.es>
2023-05-19 19:49:15 +0200

    Añade bloque <repository> (para Nexus) en pom.xml de domain

--- view/src/main/java/es/uvigo/esei/xcs/composite/view/PersonManager.java
+++ view/src/main/java/es/uvigo/esei/xcs/composite/view/PersonManager.java
@@ -12,10 +12,10 @@ import es.uvigo.esei.xcs.composite.logic.PeopleFacade;
 public class PersonManager {
 	public static void main(String[] args) {
 		// H2 database
-		/*final String url = "jdbc:h2:./target/composite";
-		final String user = "sa";
-		final String password = "";
-		*/
+//		final String url = "jdbc:h2:./target/composite";
+//		final String user = "sa";
+//		final String password = "";
+		
 		// Derby database
 		final String url = System.getProperty("db.url");
 		final String user = System.getProperty("db.login");
